c81d90931746e61d4c0f0b7fc7a5578ea763b6b4
SOULEYMANE <sdaouda@gmail.com>
2019-06-20 16:07:40 +0100

    Added files and changes required for deployment to heroku

--- ImmoCenter/settings.py
+++ ImmoCenter/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['immoniger.herokuapp.com']
+#ALLOWED_HOSTS = ['immoniger.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
